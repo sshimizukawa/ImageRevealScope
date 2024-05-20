fce2c6b1141f2a875045ec041aff7a8099890a07
shunsuke.shimizukawa <shunsuke.shimizukawa@ADS00417.local>
2024-05-21 00:40:05 +0900

    urlから画像を取得できるように変更とその他 * Loupeがconfigオブジェクトで初期化されるように変更 * 画像urlから画像を取得できるように追加 * 画像を読み込み後、画像ソースをクエリパラメータに追加する機能を追加 * その他

--- js/loupe.js
+++ js/loupe.js
@@ -1,10 +1,16 @@
 export class Loupe {
-  constructor(canvas, backgroundImage, frontImage) {
-    this.canvas = canvas;
-    this.ctx = canvas.getContext("2d");
-    this.backgroundImage = backgroundImage;
-    this.frontImage = frontImage;
+  constructor(config) {
+    // canvas設定
+    this.canvas = config.canvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.canvas.width = config.canvasWidth;
+    this.canvas.height = config.canvasHeight;
 
+    // 画像取得
+    this.backImage = config.backImage;
+    this.frontImage = config.frontImage;
+
+    // カーソル位置初期化
     this.mouseX = 0;
     this.mouseY = 0;
 
@@ -20,7 +26,7 @@ export class Loupe {
     this.ctx.fillStyle = "gray";
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
-    this.ctx.drawImage(this.backgroundImage, 0, 0);
+    this.ctx.drawImage(this.backImage, 0, 0);
 
     this.ctx.save();
     this.ctx.beginPath();
--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
-import { Loupe } from "./loupe.js";
-
-const canvas = document.getElementById("canvas");
-console.log("canvas", canvas);
+import { Loupe } from "./Loupe.js";
 
 function loadImage(src) {
   return new Promise((resolve) => {
@@ -13,11 +10,56 @@ function loadImage(src) {
   });
 }
 
-const backgroundLoaded = loadImage("assets/img/1.png");
-const frontLoaded = loadImage("assets/img/2.png");
+const windowOnLoad = async () => {
+  const currentUrl = new URL(window.location.href);
+  const searchParams = new URLSearchParams(currentUrl.search);
+  const currentBackImageUrl = searchParams.get("back_image");
+  const currentFrontImageUrl = searchParams.get("front_image");
 
-Promise.all([backgroundLoaded, frontLoaded]).then((images) => {
-  const [backgroundImage, frontImage] = images;
-  const loupe = new Loupe(canvas, backgroundImage, frontImage);
+  if (!currentFrontImageUrl || !currentBackImageUrl) return;
+
+  document.getElementById("back-image-url").value = currentBackImageUrl;
+  document.getElementById("front-image-url").value = currentFrontImageUrl;
+  document.getElementById("current-url").value = currentUrl.toString();
+
+  await playLoupe(currentBackImageUrl, currentFrontImageUrl);
+};
+
+const loadButtonOnClicked = async () => {
+  const backImageUrl = document.getElementById("back-image-url").value;
+  const frontImageUrl = document.getElementById("front-image-url").value;
+
+  const currentUrl = new URL(window.location.href);
+  const searchParams = new URLSearchParams(currentUrl.search);
+
+  searchParams.set("front_image", frontImageUrl);
+  searchParams.set("back_image", backImageUrl);
+  currentUrl.search = searchParams.toString();
+
+  window.history.pushState({}, "", currentUrl.toString());
+  document.getElementById("current-url").value = currentUrl.toString();
+
+  await playLoupe(backImageUrl, frontImageUrl);
+};
+
+const playLoupe = async (backImageUrl, frontImageUrl) => {
+  const canvas = document.getElementById("canvas");
+  const backImageLoaded = loadImage(backImageUrl);
+  const frontImageLoaded = loadImage(frontImageUrl);
+  const images = await Promise.all([backImageLoaded, frontImageLoaded]);
+  const [backImage, frontImage] = images;
+
+  const loupeConfig = {
+    canvas: canvas,
+    canvasWidth: backImage.width,
+    canvasHeight: backImage.height,
+    backImage: backImage,
+    frontImage: frontImage,
+  };
+
+  const loupe = new Loupe(loupeConfig);
   loupe.play();
-});
+};
+
+document.addEventListener("DOMContentLoaded", windowOnLoad);
+document.getElementById("load").addEventListener("click", loadButtonOnClicked);
